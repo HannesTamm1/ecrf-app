b78c0ae3c082bb1cb23189f474fb1dd0d2c070be
Hannes.tamm <hannestamm7@gmail.com>
2025-08-26 17:09:27 +0300

    Configure Vite, Tailwind and Inertia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./resources/views/**/*.blade.php", "./resources/js/**/*.vue"],
+    theme: { extend: {} },
+    plugins: [],
+};
